db581af5ec3d712ef86bcff6b225e2ee3324aed9
Joice-crypto <joyciinha_cristina@aluno.ufsj.edu.br>
2024-01-07 02:37:45 -0300

    Começando meu site pessoal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,191 @@
-import Image from 'next/image'
+/* eslint-disable react/no-unescaped-entities */
+import Image from "next/image";
+import joice from "../assets/Eu.jpeg";
+import html from "../assets/skills/icons8-html-50.png";
+import css from "../assets/skills/icons8-css-50.png";
+import js from "../assets/skills/icons8-javascript-50.png";
+import react from "../assets/skills/icons8-react-native-34.png";
+import ts from "../assets/skills/icons8-typescript-48.png";
+import next from "../assets/skills/icons8-nextjs-48.png";
+import mysql from "../assets/skills/icons8-mysql-50.png";
+import postgresql from "../assets/skills/icons8-postgreesql-50.png";
+import node from "../assets/skills/icons8-nodejs-64.png";
+import python from "../assets/skills/icons8-python-50.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="flex justify-center p-10">
+        <Image
+          className="object-cover"
+          src={joice}
+          alt=""
+          width="150"
+          height="0"
+        />
+
+        <p className="text-3xl ml-10">Hello</p>
+        <p className="text-xl mt-1 "> , I'm Joice!</p>
+      </div>
+
+      <div className=" bg-slate-900 grid grid-flow-col  py-10">
+       
+        <div className="grid grid-flow-row  mr-5 justify-center">
+          <h1 className="text-white text-xl">About Me</h1>
+          <p className="text-white w-80 text-justify mt-5">
+            Olá, sou Joice uma estudante de graduação em Ciência da Computação
+            super empolgada em me tornar uma desenvolvedora full stack. Adoro construir sites que fazem a diferença para o sucesso geral do
+            produto. Vcoê pode da uma olhada nos meus trabalhos na aba
+            'Projetos'. Além disso, sou entusiasta em criar conteúdos online
+            para ajudar iniciantes no desenvolvimento web. Conferira meu canal
+            no YouTube aqui{" "}
+          </p>
+          <a href="/projetos"><button  className="text-white m-3 p-1 shadow-xl rounded-md w-44 hover:bg-fuchsia-700 bg-fuchsia-800" >Acessar Projetos</button> </a>     
+        </div>
+
+        <div className="">
+          <p className="text-white text-xl text-center mb-5">My Skills</p>
+          <div id="skilss" className="grid grid-cols-3 ml-5 gap-y-3  mt-5">
+          <div className="">
+          <Image
+          className="object-cover hover:scale-125"
+          src={html}
+          alt=""
+          width="50"
+          height="48"
+        />
+
+          <p className="text-white ml-2">Html</p>
+          </div>
+
+          <div>
+          <Image
+          className="object-cover hover:scale-125"
+          src={css}
+          alt=""
+          width="50"
+          height="48"
+        />
+
+          <p className="text-white ml-2">CSS</p>
+          </div>
+
+          <div>
+          <Image
+          className="object-cover hover:scale-125"
+          src={js}
+          alt=""
+          width="50"
+          height="48"
+        />
+
+          <p className="text-white mt-2">JavaScript</p>
+          </div>
+
+          <div>
+          <Image
+          className="object-cover hover:scale-125"
+          src={ts}
+          alt=""
+          width="50"
+          height="48"
+        />
+
+          <p className="text-white mt-2  ">TypeScript</p>
+          </div>
+         
+          <div>
+          <Image
+          className="object-cover hover:scale-125"
+          src={react}
+          alt=""
+          width="50"
+          height="48"
+        />
+
+          <p className="text-white mt-2  ">ReactJS</p>
+          </div>
+
+          <div>
+          <Image
+          className="object-cover hover:scale-125"
+          src={next}
+          alt=""
+          width="50"
+          height="48"
+        />
+
+          <p className="text-white mt-2  ">NextJS</p>
+          </div>
+
+          <div>
+          <Image
+          className="object-cover hover:scale-125"
+          src={node}
+          alt=""
+          width="50"
+          height="48"
+        />
+
+          <p className="text-white mt-2  ">NodeJs</p>
+          </div>
+
+          <div>
+          <Image
+          className="object-cover hover:scale-125"
+          src={python}
+          alt=""
+          width="50"
+          height="48"
+        />
+
+          <p className="text-white mt-2  ">Python</p>
+          </div>
+
+
+
+          <div>
+          <Image
+          className="object-cover hover:scale-125"
+          src={mysql}
+          alt=""
+          width="50"
+          height="48"
+        />
+
+          <p className="text-white mt-2  ">MySQL</p>
+          </div>
+
+          <div>
+          <Image
+          className="object-cover hover:scale-125"
+          src={postgresql}
+          alt=""
+          width="50"
+          height="48"
+        />
+
+          <p className="text-white mt-2  ">PostgreSQL</p>
+          </div>
+
+          </div>
+          
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    <div className=" bg-gray-dark  justify-items-center  pt-10  ">
+    <h1 className="text-white text-center text-xl">Contact</h1>
+      <form className=" p-5 grid grid-flow-row w-7/12 ml-64 grid-cols-1 " action="">
+        <label className="text-white pb-3" htmlFor="nome">Nome:</label>
+        <input className="rounded-md" type="text" />
+        <label className="text-white  py-3 " htmlFor="email">Email:</label>
+        <input className="rounded-md" type="email" />
+        <label className="text-white mt-5" htmlFor="mensagem">Digite sua mensagem:</label>
+        <textarea className=" h-32 mt-5 rounded-md"></textarea>
+        <input className=" mt-7 w-40 bg-fuchsia-800 hover:bg-fuchsia-700 shadow-xl ml-80 text-white rounded-lg py-2  " type="button" value="Enviar" />
+      </form>
+    </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,58 @@
+import insta from "../assets/icons8-instagram-48.png";
+import github from "../assets/icons8-github-50.png";
+import linkedin from "../assets/icons8-linkedin-48.png";
+import Image from "next/image";
+export function Footer() {
+  return (
+    <footer className=" bg-black h-44 bottom-0 md:max-xl:flex grid grid-rows-1 grid-flow-row">
+      
+      <ul className="grid grid-rows-2  ml-20 grid-flow-col">
+        <p className="text-white font-bold mt-7">Joice Cristina</p>
+        <p className="text-white mt-6 ">
+        joicecristina059@gmail.com
+        </p>
+
+        <h2 className="text-white mt-7 ml-96 font-bold">Midias Sociais</h2>
+        <div className=" grid grid-flow-col mt-5 ml-96 justify-start ">
+          <a href="https://www.instagram.com/joice_crsilva/">
+            <Image
+              className="object-cover  "
+              src={insta}
+              alt=""
+              width="32"
+              height="40"
+            />
+          </a>
+
+          <a href="https://www.linkedin.com/in/joice-cristina-3451851a4/">
+            <Image
+              className="object-cover mr-2 "
+              src={linkedin}
+              alt=""
+              width="32"
+              height="40"
+            />
+          </a>
+          <a href="https://github.com/Joice-crypto">
+            <Image
+              className="object-cover mr-2 "
+              src={github}
+              alt=""
+              width="32"
+              height="40"
+            />
+          </a>
+        </div>
+      </ul>
+      
+        <div className=" mt-5 grid grid-flow-col">
+        <hr className=" bg-white ml-10 mr-10" />
+        </div>
+     
+
+      <p className="text-white mb-9 mt-5 text-center ">
+        &copy; Copyright 2024. Made by Joice Cristina
+      </p>
+    </footer>
+  );
+}
